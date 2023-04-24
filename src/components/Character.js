a4a6246e8c75839717fbc2c2b5ed3391730ddc00
becca1d1 <rebeccacshumway@gmail.com>
2023-04-23 23:38:16 -0700

    push

--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,18 @@
 // Write your Character component here
+import React, { useState, useEffect } from 'react';
+
+function Character({characters}) {
+    console.log(characters);
+    return (
+        <div className="characterList">
+            <ul>
+            {characters.name.map((names) => {
+                return <li key={names}></li>
+            })}
+            </ul>
+        </div>
+    );
+}
+//research optional chaining
+// how do i delay a rendering of a component while youre waiting on an API call?
+export default Character;
